fa013f2cfd73d7b5079f33680d65b5bd3c213483
Zhuo Biao Cai <114125018+208cai5099@users.noreply.github.com>
2023-05-31 13:57:22 -0400

    Update firebase.js
    Added restriction to only allow seas.upenn.edu users to sign up

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -21,4 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();;
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomerParameters({hd: "seas.upenn.edu"});
