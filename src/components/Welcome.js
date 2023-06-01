45dcac5ed3d1e84700a6c0c52effe7df6bc85988
Ben Swanson <benjaminmswanson@gmail.com>
2023-06-01 17:41:50 -0400

    Contact and welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,9 +3,30 @@ import { Button } from "rsuite";
 export function Welcome () {
     return (
         <>
-            <h1>
-                Introduction page for when user has not selected anything yet. 
-            </h1>
+            <h3>
+            Welcome to Penn Resources 
+            </h3>
+            <p>
+            Select a course and a module to find supplemental resources to help strengthen your understanding of course material
+            </p>
+
+            <h4>
+            Consider Adding a Resource            
+            </h4>
+            <p>
+            To help other students, please add resources that helped you learn course material
+            </p>
+            <p>
+            These can include YouTube videos, articles, textbooks, or anything else that you found online that made course material easier to understand.
+            </p>
+
+            <h4>
+            Student Conduct Rules Apply            
+            </h4>
+            <p>
+            Be aware of what you’re posting and reading and make sure that it doesn’t violate any MCIT academic policies. Thank you for contributing!
+            </p>
+
         </>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,47 @@
-import { Button } from "rsuite";
+import { Panel, Placeholder, Row, Col } from 'rsuite';
+
+const Card = props => (
+  <Panel {...props} bordered header="Lea Emerlyn">
+    <p>leaemerlyn@seas.upenn.edu</p>
+  </Panel>
+);
+
+const Card1 = props => (
+  <Panel {...props} bordered header="Ben Swanson">
+    <p>bswan1@seas.upenn.edu</p>
+  </Panel>
+);
+
+const Card2 = props => (
+  <Panel {...props} bordered header="Abdullah Amer">
+    <p>abamer@seas.upenn.edu</p>
+  </Panel>
+);
+
+const Card3 = props => (
+  <Panel {...props} bordered header="Leven Cai">
+    <p>levencai@seas.upenn.edu</p>
+  </Panel>
+);
+
 
 export function Contact () {
     return (
         <>
-            <h1>
-                Contact Page
-            </h1>
-            <Button>hello</Button>
+            <Row>
+              <Col md={6} sm={12}>
+                <Card />
+              </Col>
+              <Col md={6} sm={12}>
+                <Card1 />
+              </Col>
+              <Col md={6} sm={12}>
+                <Card2 />
+              </Col>
+              <Col md={6} sm={12}>
+                <Card3 />
+              </Col>
+            </Row>
         </>
     )
 }
