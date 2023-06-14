cbd12e28fd6f6583535dbece82ad3a08f5d440b0
Ben Swanson <benjaminmswanson@gmail.com>
2023-06-14 16:00:59 -0400

    New Welcome Page Design

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,13 @@
-import { Button } from "rsuite";
 import './Welcome.css'
+import React from 'react';
+import './styles.css';
+
+
+import { RadioTile, RadioTileGroup } from 'rsuite';
+import { Icon } from '@rsuite/icons';
+import SearchIcon from '@rsuite/icons/Search';
+import { VscRepo } from 'react-icons/vsc';
+import SendIcon from '@rsuite/icons/Send';
 
 export function Welcome () {
     return (
@@ -7,27 +15,18 @@ export function Welcome () {
             <h3 className='test'>
             Welcome to Penn Resources 
             </h3>
-            <p>
-            Select a course and a module to find supplemental resources to help strengthen your understanding of course material
-            </p>
-
-            <h4>
-            Consider Adding a Resource            
-            </h4>
-            <p>
-            To help other students, please add resources that helped you learn course material
-            </p>
-            <p>
-            These can include YouTube videos, articles, textbooks, or anything else that you found online that made course material easier to understand.
-            </p>
-
-            <h4>
-            Student Conduct Rules Apply            
-            </h4>
-            <p>
-            Be aware of what you’re posting and reading and make sure that it doesn’t violate any MCIT academic policies. Thank you for contributing!
-            </p>
-
+            <RadioTileGroup defaultValue="title">
+                <RadioTile icon={<Icon as={SearchIcon} />} label="Navtigation">
+                Select a course and a module to find supplemental resources to help strengthen your understanding of course material
+                </RadioTile>
+                <RadioTile icon={<Icon as={SendIcon} />} label="Adding a Resource">
+                To help other students, please add resources that helped you learn course material.
+                These can include YouTube videos, articles, textbooks, or anything else that you found online that made course material easier to understand.
+                </RadioTile>
+                <RadioTile icon={<Icon as={VscRepo} />} label="Academic Integrity">
+                Be aware of what you’re posting and reading and make sure that it doesn’t violate any MCIT academic policies. Thank you for contributing!
+                </RadioTile>
+            </RadioTileGroup>
         </>
     )
 }
