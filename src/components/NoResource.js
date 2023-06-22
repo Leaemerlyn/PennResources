2650ccbacf3432462c4e25af0f22816a0f6a09d8
benmswanson <113471340+benmswanson@users.noreply.github.com>
2023-06-22 15:07:27 -0400

    Update NoResource.js

--- src/components/NoResource.js
+++ src/components/NoResource.js
@@ -1,9 +1,13 @@
+import { Panel } from 'rsuite';
+
 export function NoResource () {
     return (
         <>
-            <h1>
-                When there are no resources uploaded.
-            </h1>
+        <Panel header="No Resources Posted (yet)">
+            <p>
+              Post an article, video, or other resource that helped you better understand course material!
+              </p>
+          </Panel>
         </>
     )
 }
