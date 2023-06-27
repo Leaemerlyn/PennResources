7983e92d616167763f80481094cec81165255ec6
aamer2 <abdullahamer58@gmail.com>
2023-06-27 12:40:37 -0400

    Added Course and Module filter for user

--- src/pages/MyContributions.js
+++ src/pages/MyContributions.js
@@ -1,73 +1,130 @@
-import { Button } from 'rsuite';
-import "./MyContributions.css"
+import { Button, SelectPicker } from 'rsuite';
+import "./MyContributions.css";
 import { ContributionCard } from '../components/ContributionCard';
 import { useState, useEffect } from 'react';
-import { Contribute } from './Contribute';
-import { collection, query, where, getDocs, QuerySnapshot } from 'firebase/firestore';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 import { database, auth } from "../config/firebase";
+import { Contribute } from './Contribute';
 
-const course = ["591", "592", "593", "594", "595", "596", "515", "521", "530", "545", "547", "549", "550", "551", "553", "555", "581", "582", "575", "541", "542", "546"].map(item => ({ label: item, value: item }));
-const module = ["Module 1", "Module 2", "Module 3", "Module 4", "Module 5", "Module 6", "Module 7", "Module 8", "Module 9", "Module 10", "Module 11", "Module 12", "Module 13"].map(item => ({ label: item, value: item }));
+const courseOptions = ["591", "592", "593", "594", "595", "596", "515", "521", "530", "545", "547", "549", "550", "551", "553", "555", "581", "582", "575", "541", "542", "546"].map(item => ({ label: item, value: item }));
+const moduleOptions = ["Module 1", "Module 2", "Module 3", "Module 4", "Module 5", "Module 6", "Module 7", "Module 8", "Module 9", "Module 10", "Module 11", "Module 12", "Module 13"].map(item => ({ label: item, value: item }));
+const resourceTypeOptions = ["Video", "Reading", "Practice Problem"].map(item => ({ label: item, value: item }));
 
-const resourceType = ["Video", "Reading", "Practice Problem"].map(item =>({label: item, value: item})); 
+export function MyContributions() {
+  const [contributionsList, setContributionsList] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [selectedModule, setSelectedModule] = useState(null);
 
-export function MyContributions () {
-    // represents the list of contributions by the current user
-    const [contributionsList, setContributionsList] = useState([]);
+  useEffect(() => {
+    const getContributions = async () => {
+      const currentUser = auth.currentUser;
+      const currentUID = currentUser.uid;
+      const resourcesCollection = collection(database, "resources");
+      let queryOnUID = query(resourcesCollection, where("uid", "==", currentUID));
 
-    // queries the database for the user's contributions by UID
-    // for each contribution, store the data in an array and save the array
-    // in the contributionsList from above
-    useEffect(() => {
-        const getContributions = async() => {
-    
-            const currentUser = auth.currentUser;
-            const currentUID = currentUser.uid;
-            const resourcesCollection = collection(database, "resources");
-            const queryOnUID = query(resourcesCollection, where("uid", "==", currentUID));
-            const queryUIDSnapshot = await getDocs(queryOnUID);
+      if (selectedCourse && selectedModule) {
+        queryOnUID = query(
+          resourcesCollection,
+          where("uid", "==", currentUID),
+          where("Course", "==", selectedCourse),
+          where("Module", "==", selectedModule)
+        );
+      } else if (selectedCourse) {
+        queryOnUID = query(
+          resourcesCollection,
+          where("uid", "==", currentUID),
+          where("Course", "==", selectedCourse)
+        );
+      } else if (selectedModule) {
+        queryOnUID = query(
+          resourcesCollection,
+          where("uid", "==", currentUID),
+          where("Module", "==", selectedModule)
+        );
+      }
 
-            // use the docs method to get the array of documents objects from the snapshot
-            // use the map method to add each document object's fields to the array userContributions
-            const userContributions = queryUIDSnapshot.docs.map((contributionDoc) => ({
-                ...contributionDoc.data(), id: contributionDoc.id
-            }));
-            
-            // save the array of user contributions in the state above
-            setContributionsList(userContributions);
-        };
+      const queryUIDSnapshot = await getDocs(queryOnUID);
+      const userContributions = queryUIDSnapshot.docs.map((contributionDoc) => ({
+        ...contributionDoc.data(),
+        id: contributionDoc.id
+      }));
 
-        // call on the function to get the user contributions
-        getContributions();
+      setContributionsList(userContributions);
+    };
 
-    }, [])
+    getContributions();
+  }, [selectedCourse, selectedModule]);
 
-    const [addingResource, setAddingResource] = useState(false);
-    const [showCard, setShowCard] = useState(true);
+  const [addingResource, setAddingResource] = useState(false);
+  const [showCard, setShowCard] = useState(true);
 
+  const handleCourseFilter = (value) => {
+    setSelectedCourse(value);
+  };
 
-    return(
-        <div className="contributionCardContainer">
-          {addingResource ? (
-            <></>
-          ) : (
-            <h4>
-              {contributionsList.length === 0
-                ? "No resources found. Click below to start adding resources."
-                : "Here are all your contributions to Penn Resources"}
-            </h4>
-          )}
-          {addingResource ? (
-            <Contribute setAddingResource={setAddingResource} />
-          ) :  
-            <div>
-                {contributionsList.map((contribution) => (
-                <ContributionCard showCard = {showCard} setShowCard = {setShowCard} course = {contribution.Course} module = {contribution.Module} link = {contribution.Link} description = {contribution.Description} title={contribution.Title} 
-                anonymity={contribution.Anonymity} type={contribution.Type} docID={contribution.id}/>
-            ))}
-            </div>
-            }
-            {addingResource ? <></> : <Button appearance='ghost' className="addResource" onClick={() => setAddingResource(true)}>Add Resources</Button>}
+  const handleModuleFilter = (value) => {
+    setSelectedModule(value);
+  };
+
+  return (
+    <div className="contributionCardContainer">
+      {addingResource ? (
+        <></>
+      ) : (
+        <h4>
+          {contributionsList.length === 0
+            ? "No resources found. Click below to start adding resources."
+            : "Here are all your contributions to Penn Resources"}
+        </h4>
+      )}
+      {addingResource ? (
+        <Contribute setAddingResource={setAddingResource} />
+      ) : (
+        <div>
+          <div className="filterContainer">
+            <SelectPicker
+              data={courseOptions}
+              value={selectedCourse}
+              onChange={handleCourseFilter}
+              placeholder="Select"
+              className="filterInput"
+            />
+            <SelectPicker
+              data={moduleOptions}
+              value={selectedModule}
+              onChange={handleModuleFilter}
+              placeholder="Select"
+              className="filterInput"
+            />
+          </div>
+          {contributionsList.map((contribution) => (
+            <ContributionCard
+              key={contribution.id}
+              showCard={showCard}
+              setShowCard={setShowCard}
+              course={contribution.Course}
+              module={contribution.Module}
+              link={contribution.Link}
+              description={contribution.Description}
+              title={contribution.Title}
+              anonymity={contribution.Anonymity}
+              type={contribution.Type}
+              docID={contribution.id}
+            />
+          ))}
         </div>
-    )
+      )}
+      {addingResource ? (
+        <></>
+      ) : (
+        <Button
+          appearance="ghost"
+          className="addResource"
+          onClick={() => setAddingResource(true)}
+        >
+          Add Resources
+        </Button>
+      )}
+    </div>
+  );
 }
