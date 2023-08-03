a50595a3cb3c7f61a599bd81ec41e6697aa2a3e8
aamer2 <abdullahamer58@gmail.com>
2023-08-02 22:34:16 -0400

    like button updates

--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -1,34 +1,42 @@
-import { Panel, Placeholder, Tag, IconButton } from "rsuite";
-import { FaRegHeart, FaHeart } from "react-icons/fa";
-import "./ResourceCard.css"
+import { Panel, Tag } from "rsuite";
+import { FaThumbsUp, FaRegThumbsUp } from "react-icons/fa"; // Importing thumbs-up icons
+import "./ResourceCard.css";
 import { useState } from "react";
 
-export function ResourceCard ({course, description, link, title, type, contributor}) {
-    const [hover, setHover] = useState(false);
-    const [liked, setLiked] = useState(false);
+export function ResourceCard({ course, description, link, title, type, contributor }) {
+  const [hover, setHover] = useState(false);
+  const [liked, setLiked] = useState(false);
 
-    const toggleLike = (event) => {
-        event.stopPropagation();
-        setLiked(!liked);
-    };
 
-    const customHeader = (
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <p style={{ margin: 0, fontWeight: 'normal' }}>{title}</p>
-            <p style={{ margin: 0, fontSize: '0.8em', textAlign: 'right' }}>{contributor}</p>
+
+  
+  const toggleLike = (event) => {
+    event.stopPropagation();
+    setLiked(!liked);
+  };
+  
+
+  const customHeader = (
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <p style={{ margin: 0, fontWeight: 'normal' }}>{title}</p>
+      <p style={{ margin: 0, fontSize: '0.8em', textAlign: 'right' }}>{contributor}</p>
+    </div>
+  );
+
+  return (
+    <div className="resourceCard">
+      <Panel header={customHeader} bordered onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} shaded={hover} style={{ cursor: 'pointer' }} onClick={() => window.open(link, '_blank', 'noreferrer')}>
+        <p>{description}</p>
+        <br></br>
+        <div className="bottomInfo" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Tag>{type}</Tag>
+          <div onClick={toggleLike} style={{ cursor: 'pointer' }}>
+            {liked ? <FaThumbsUp style={{ color: 'robin blue', size: '0.8em' }} /> : <FaRegThumbsUp size='0.8em' />}
+          </div>
         </div>
-    )
-
-    return (
-        <div className="resourceCard">
-            <Panel header={customHeader} bordered onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} shaded={hover} style={{cursor:'pointer'}} onClick={() => window.open(link, '_blank', 'noreferrer')}>
-                <p>{description}</p>
-                <br></br>
-                <div className="bottomInfo" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <Tag>{type}</Tag>
-                    <IconButton icon={<div>{liked ? <FaHeart style={{color:'red', size: '0.8em'}}/> : <FaRegHeart size='0.8em' />}</div>} circle size="sm" onClick={toggleLike} />
-                </div>
-            </Panel>
-        </div>
-    )
+      </Panel>
+    </div>
+  );
 }
+
+
